1bb03f3feffa797f61e7cab611f4b766e84cf78d
Martynas <martynas.bartnykas@gmail.com>
2023-12-09 23:27:36 +0200

    Tried to implement solutions for all tasks, however was unsuccessful.

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.module3.task4;
 
+import static java.lang.Math.pow;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+    char A = 1;
+    char B = 1;
+
+
+        double v = ((((9 * pow(A, 2)) - (5 * B)) + 2 + A) - 7) * ((double) ((A + B) - (4 * A * B)) / 2);
+
+        System.out.println(v);
     }
 }
